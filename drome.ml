59115e2769f6465a761ca5ebff5002d30570b319
nasadorian <nasadorian@salesforce.com>
2021-04-30 18:21:08 -0700

    implement resource, not tested

--- drome.ml
+++ drome.ml
@@ -8,7 +8,7 @@ module IO = struct
   include IOInstances
 
   (* suspend a -- lifts a deferred action into the IO context
-   * This is the main entry point for most IO programs *)
+   * main starting point for most IO programs *)
   let suspend (a : 'a thunk) : 'a io = Suspend a
 
   (* unsafe_run_sync -- executes an IO program synchronously
@@ -52,11 +52,47 @@ module IO = struct
     Suspend
       (fun _ -> try Result.ok (unsafe_run_sync io) with e -> Result.error e)
 
-  (* unsafe_run_async io cb -- executes IO program in another thread; call
-   * callback cb when finished executing *)
-  let unsafe_run_async (io : 'a io) (cb : 'a -> 'b) : Thread.t =
+  (* unsafe_run_async io cb -- executes IO program in another thread; calling
+   * callback `cb` when finished executing *)
+  let unsafe_run_async (cb : 'a -> 'b) (io : 'a io) : Thread.t =
     Thread.create (cb << unsafe_run_sync) io
 
   (* unsafe_run_async' io -- executes IO program in another thread no callback *)
-  let unsafe_run_async' (io : 'a io) : Thread.t = unsafe_run_async io id
+  let unsafe_run_async' (io : 'a io) : Thread.t = unsafe_run_async id io
+end
+
+module Resource = struct
+  include Io_base
+
+  let make (acq : 'a io) (rel : 'a -> unit io) : 'a resource =
+    Allocate (rel, acq)
+
+  let bind f r = RBind (f, r)
+
+  let pure (a : 'a) : 'a resource = RPure a
+
+  let map (f : 'a -> 'b) (r : 'a resource) : 'b resource =
+    let f' a = pure (f a) in
+    RBind (f', r)
+
+  let kleisli f g a = bind g (f a)
+
+  let rec use : type a b. (a -> b) -> a resource -> b io =
+   fun u r ->
+    match r with
+    | Allocate (release, acquire) ->
+        IO.(
+          acquire >>= fun a ->
+          let out = u a in
+          release a *> pure out)
+    | RBind (f, res) -> (
+        match res with
+        | Allocate (release, acquire) ->
+            IO.(
+              acquire >>= fun a ->
+              let res' = f a in
+              release a *> use u res')
+        | RBind (g, res') -> use u (RBind (kleisli g f, res'))
+        | RPure a -> use u (f a))
+    | RPure a -> IO.pure (u a)
 end
--- io_base.ml
+++ io_base.ml
@@ -8,3 +8,8 @@ type _ io =
   | RaiseError : exn -> 'a io
   | Attempt : 'a io -> ('a, exn) result io
   | HandleErrorWith : ((exn -> 'a io) * 'a io) -> 'a io
+
+type _ resource =
+  | Allocate : (('a -> unit io) * 'a io) -> 'a resource
+  | RPure : 'a -> 'a resource
+  | RBind : (('a -> 'b resource) * 'a resource) -> 'b resource
